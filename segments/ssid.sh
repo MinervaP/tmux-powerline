3f492742a475b338ccfc2a0c9817f6866b2f1e02
MinervaP <GoddessMinervaP@gmail.com>
2016-04-02 22:05:34 +0900

    ssidのセグメント オフラインの時にも表示できるようにした

--- segments/ssid.sh
+++ segments/ssid.sh
@@ -1,3 +1,4 @@
 run_segment() {
-	echo $(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I |grep "\sSSID"|sed -e "s/[^:]*: //")
+	ssid=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/A/Resources/airport -I |grep "\sSSID"|sed -e "s/[^:]*: //")
+	echo ${ssid:-"no_wifi"}
 }
